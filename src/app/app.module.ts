5dda9f431f34599d868c233d7322dc6aeefe1cd3
jinyang <yang.youth1020@gmail.com>
2018-07-09 15:48:09 +0800

    Fix hom-ts file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { ThemeableBrowser } from '@ionic-native/themeable-browser';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    ThemeableBrowser
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ThemeableBrowser, ThemeableBrowserOptions, ThemeableBrowserObject } from '@ionic-native/themeable-browser';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,40 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private themable: ThemeableBrowser) {
 
   }
 
+  openBrowser() {
+    const options: ThemeableBrowserOptions = {
+      toolbar: {
+        height: 44,
+        color: '#3573bbff'
+      },
+      title: {
+        color: '#ffffffff',
+        showPageTitle: true,
+        staticText: 'Dmd browser'
+      },
+      backButton: {
+        wwwImage: 'assets/imgs/back.png',
+        align: 'left',
+        event: 'backPressed'
+      },
+      forwardButton: {
+        wwwImage: 'assets/imgs/forward.png',
+        align: 'right',
+        event: 'forwardPressed'
+      },
+      closeButton: {
+        wwwImage: 'assets/imgs/close.png',
+        align: 'left',
+        event: 'closePressed'
+      },
+    }
+    const browser: ThemeableBrowserObject = this.themable.create('https://ionicacademy.com', '_blank', options);
+    browser.on('closePressed').subscribe(res => {
+      browser.close();
+    });
+  }
 }
